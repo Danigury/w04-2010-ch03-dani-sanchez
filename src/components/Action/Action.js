f45d366128248ad59632f483fa07d934a33ca3ba
Dani Sanchez <danysq87@gmail.com>
2021-10-21 19:10:14 +0200

    Bug fixes from chrome developer tools

--- src/components/Action/Action.js
+++ src/components/Action/Action.js
@@ -2,14 +2,13 @@ import PropTypes from "prop-types";
 
 function Action({ href, text, className, actionOnClick }) {
   return (
-    <a href={href} className={className} actionOnClick={actionOnClick}>
+    <a href={href} className={className} onClick={actionOnClick}>
       {text}
     </a>
   );
 }
 
+export default Action;
 Action.propTypes = {
   actionOnClick: PropTypes.func.isRequired,
 };
-
-export default Action;
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -3,7 +3,7 @@ function Info({ isCalling }) {
   return <span className="message">{isCalling ? "Calling..." : ""}</span>;
 }
 
-Info.propTypes = {
-  isCalling: PropTypes.string.isRequired,
-};
 export default Info;
+Info.propTypes = {
+  isCalling: PropTypes.bool.isRequired,
+};
